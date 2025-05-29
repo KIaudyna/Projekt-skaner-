9633b3fccad05b4566a22df61bff6887fcc6455a
Gzylka <mariagzyl6@gmail.com>
2025-05-30 01:48:11 +0200

    baza alkoholi do pliku csv narazie tylko zdefiniowana funkcja mozna przerobic zeby pytalo czy to wsztko

--- alfa_ver.py
+++ alfa_ver.py
@@ -32,14 +32,22 @@ class BazaAlkoholi:
     def pokaz_wszystkie(self):
         for alkohol in self.rekordy:
             print(alkohol)
+#dodawanie do bazy w excelu jak potraficie łatwiej to dawać to jest moj pomysł
+def dodawanie_alkoholi_do_bazy(id, data, nazwa, waga_pusta, waga_zwazona, ilosc_pelnych_butelek):
+    dane=str(id)+";"+str(data)+";"+str(nazwa)+";"+str(waga_pusta)+";"+str(waga_zwazona)+";"+str(ilosc_pelnych_butelek)+"\n"
+    baza_tekstowa=open("baza_alko_excel.csv","w")
+    baza_tekstowa.write(dane)
+#przy zczytywaniu trzeba pamietac zeby zmienac to co  trzeba na inty oraz usuwav \n z koncow.
 
 
 # === Przykładowe użycie ===================================================================
 if __name__ == "__main__":
     baza = BazaAlkoholi()
-
+#JESLI CHCEMY BAZE TO TRZEBA BEZ POLSKICH ZNAKOW!!!!!
     data= date.today()
-    alkohol1 = Alkohol(1, data, "Wóda Żubrówka", 0.5, 1.2, 0)
+    alkohol1 = Alkohol(1, data, "Woda Zubrowka", 0.5, 1.2, 0)
+    dodawanie_alkoholi_do_bazy(1, data, "Woda Zubrowka", 0.5, 1.2, 0)
+    
     alkohol2 = Alkohol(2, data, "Whisky Jack Daniels", 0.7, 1.9, 3)
     alkohol3 = Alkohol(3, data, "Piwo Heineken", 0.3, 0.85, 4)
     alkohol4 = Alkohol(3, data, "Piwo Warka Strong", 0.3, 0.85, 7)
